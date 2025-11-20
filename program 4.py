0c515308e64a82d3f4b2d964f6f8e6fa1edfc2ad
maliklubna <75310080+maliklubna@users.noreply.github.com>
2025-11-20 16:25:16 +0530

    Add files via upload

---
+++ program 4.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first integer:\n"))
+b = int(input("Enter second integer:\n"))
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b  
+modulus = a%b
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulus:", modulus)
